2b89d0c34f0f677dd2cf66a66f4e0d2c16a028ea
wave-tv <42773419+wave-tv@users.noreply.github.com>
2018-10-01 08:43:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,7 @@ import App from './pages/App';
 import registerServiceWorker from './registerServiceWorker';
 
 render((
-  
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-126666128-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-126666128-1');
-</script>
-       
+         
   <BrowserRouter>
     <App />
   </BrowserRouter>
