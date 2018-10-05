7c8352965e32a6566bf37b1f260aedf0228ff580
Connor Markwell <markwell.connor@gmail.com>
2018-10-05 12:54:31 -0400

    Remove unused Fragment import

--- src/pages/components/player/player.js
+++ src/pages/components/player/player.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './player.css';
 import firebase from "./firebase.js";
