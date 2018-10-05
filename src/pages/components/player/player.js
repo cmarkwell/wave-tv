e5d06b9e00ab7ce48701e20d60e3bc79aab50ed4
Connor Markwell <markwell.connor@gmail.com>
2018-10-05 12:49:03 -0400

    Autoplay videos one after the other

--- src/pages/components/player/player.js
+++ src/pages/components/player/player.js
@@ -1,31 +1,38 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './player.css';
 import firebase from "./firebase.js";
 import YouTube from 'react-youtube';
 import Back from './back.png';
 
+const VIDEO_OPTS = {
+  height: '390',
+  width: '640',
+  playerVars: { // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+    controls: 0,
+    showinfo: 0,
+    modestbranding: true,
+    showsearch:0,
+    rel:0
+  },
+};
+
 class App extends Component {
   constructor() {
-
     super();
     this.state = {
-      currentItem: "",
+      playerIndex: 0, // Index of currently playing video in news array
       username: "",
-      News: []
+      news: []        // Array of video data from firebase
     };
-    this.handleChange = this.handleChange.bind(this);
   }
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
+
   componentDidMount() {
     const NewsRef = firebase
       .database()
-      .ref("News")
-      .limitToLast(1);
+      .ref('News')
+      .orderByChild('date');
     NewsRef.on("value", snapshot => {
       let News = snapshot.val();
       let newState = [];
@@ -39,47 +46,54 @@ class App extends Component {
           videoid: News[item].videoid
         });
       }
+
       this.setState({
-        News: newState
+        news: newState
       });
     });
   }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   removeItem(itemId) {
     const itemRef = firebase.database().ref(`/News/${itemId}`);
     itemRef.remove();
   }
+
+  playNextVideo = () => {
+    this.setState(prevState => {
+      const { news, playerIndex } = prevState;
+      return {
+          playerIndex: playerIndex < news.length ? playerIndex + 1 : 0,
+      };
+    });
+  }
+
   render() {
-    const opts = {
-          height: '390',
-          width: '640',
-          playerVars: { // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
-          controls: 0,
-          showinfo: 0,
-          modestbranding: true,
-          showsearch:0,
-          rel:0
-          }
-        };
+    const currentVideo = this.state.news[this.state.playerIndex];
+
     return (
       <div className="app">
         <div className="container">
         <Link to="/"><img src={Back} className="backButton"/></Link>
         <h1><mark>News</mark></h1>
           <section className="display-item">
-            <div className="wrapper">
-              <ul>
-                {this.state.News.map(item => {
-                  return (
-                    <li key={item.id}>
-                    <YouTube className="player" videoId= {item.videoid} opts={opts}/>
-                      <h2>{item.title}</h2>
-                      <p class="channel">video by: {item.channel}</p>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+            {currentVideo && 
+              <div className="wrapper">
+                <YouTube
+                  className="player"
+                  videoId={currentVideo.videoid}
+                  opts={VIDEO_OPTS}
+                  onEnd={this.playNextVideo}
+                />
+                <h2>{currentVideo.title}</h2>
+                <p className="channel">Video by: {currentVideo.channel}</p>
+              </div>
+            }
           </section>
         </div>
       </div>
